cce79bc2a3970c730651a2016a42cbc6359ec09f
unknown <mcbrooks@pm.me>
2022-10-15 18:32:29 -0400

    minor edits to base files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Header";
+import NavBar from "./components/NavBar";
+import Intro from "./components/Intro";
+import Footer from "./components/Footer";
+import Component1 from "./components/Component1";
+import Component2 from "./components/Component2";
+import Component3 from "./components/Component3";
+import Component4 from "./components/Component4";
+import About from "./components/pages/About";
 
 function App() {
+  const [currentPage, setCurrentPage] = useState("Intro");
+
+  // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
+  const renderPage = () => {
+    if (currentPage === "Intro") {
+      return <Intro />;
+    }
+    if (currentPage === "About") {
+      return <About />;
+    }
+  };
+
+  const handlePageChange = (page) => setCurrentPage(page);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Intro />} />
+          <Route path="/main" element={<Main />} />
+        </Routes>
+      </BrowserRouter> */}
+      <Header />
+      <NavBar />
+      <Intro />
+      <Component1 />
+      <Component2 />
+      <Component3 />
+      <Component4 />
+      {/* <Footer /> */}
     </div>
   );
 }
