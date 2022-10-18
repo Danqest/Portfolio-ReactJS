c8408b7657133f2bcc7cd43aabc4d29b11c1f5a4
unknown <mcbrooks@pm.me>
2022-10-17 21:15:26 -0400

    modified App files

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import {
+  Animator,
+  ScrollContainer,
+  ScrollPage,
+  Sticky,
+  Fade,
+  batch,
+  MoveOut,
+  FadeIn,
+} from "react-scroll-motion";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import "./App.css";
+import Aos from "aos";
+import "aos/dist/aos.css";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import Intro from "./components/Intro";
@@ -12,6 +24,10 @@ import Component4 from "./components/Component4";
 import About from "./components/pages/About";
 
 function App() {
+  useEffect(() => {
+    Aos.init({ duration: 1000 });
+  }, []);
+
   const [currentPage, setCurrentPage] = useState("Intro");
 
   // This method is checking to see what the value of `currentPage` is. Depending on the value of currentPage, we return the corresponding component to render.
@@ -28,20 +44,43 @@ function App() {
 
   return (
     <div className="App">
-      {/* <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Intro />} />
-          <Route path="/main" element={<Main />} />
-        </Routes>
-      </BrowserRouter> */}
-      <Header />
-      <NavBar />
-      <Intro />
-      <Component1 />
-      <Component2 />
-      <Component3 />
-      <Component4 />
-      {/* <Footer /> */}
+      <ScrollContainer>
+        <ScrollPage page={0}>
+          <Animator animation={batch(Sticky(50, 25), Fade(), MoveOut(0, -200))}>
+            <Intro />
+          </Animator>
+        </ScrollPage>
+        <ScrollPage page={1}>
+          <div className="section-1">
+            <Animator
+              animation={batch(Sticky(50, 25), Fade(), MoveOut(0, -200))}
+            >
+              <Component1 />
+            </Animator>
+          </div>
+        </ScrollPage>
+
+        <ScrollPage page={2}>
+          <div className="section-2">
+            <Animator
+              animation={batch(Sticky(50, 25), Fade(), MoveOut(0, -200))}
+            >
+              <Component2 />
+            </Animator>
+          </div>
+        </ScrollPage>
+
+        <ScrollPage page={3}>
+          <div className="section-3">
+            <Animator animation={batch(Sticky(50, 25), FadeIn())}>
+              <Component3 />
+            </Animator>
+            <Animator animation={batch(Sticky(50, 87.5), FadeIn())}>
+              <Footer />
+            </Animator>
+          </div>
+        </ScrollPage>
+      </ScrollContainer>
     </div>
   );
 }
