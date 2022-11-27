7bccdfb04bf656780b0dfdb6e867fc026ea9fbeb
unknown <mcbrooks@pm.me>
2022-11-27 13:41:31 -0500

    updated component pages for presentation

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import Carousel from "react-bootstrap/Carousel";
 import Container from "react-bootstrap/Container";
 
 import Image from "react-bootstrap/Image";
@@ -6,12 +7,12 @@ import { Card } from "react-bootstrap";
 import { Spring } from "react-spring";
 import { animated } from "react-spring";
 
-export class About extends Component {
+export class Home extends Component {
   render() {
     return (
       <div>
         <div style={c1Style}>
-          <h1 id="about">ABOUT ME</h1>
+          <h1 id="home">HOME</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
             non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
@@ -29,4 +30,4 @@ const c1Style = {
   marginTop: "20vh",
 };
 
-export default About;
+export default Home;
--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -2,32 +2,42 @@ import React, { Component } from "react";
 import { Card } from "react-bootstrap";
 import { Spring } from "react-spring";
 import { animated } from "react-spring";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Container from "react-bootstrap/Container";
+import Image from "react-bootstrap/Image";
 
-export class Contact extends Component {
+export class Projects extends Component {
   render() {
     return (
       <div>
         {/* <Card border="light" style={{ width: "18rem" }}>
           <Card.Body> */}
-        <div style={c3Style}>
-          <h1 id="contact">GET IN CONTACT</h1>
+        <div style={c2Style}>
+          <h1 id="projects">PROJECT PORTFOLIO</h1>
           <p>
             Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
             non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
             reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
             Voluptatum ducimus voluptates voluptas?
           </p>
+          <Container>
+            <Row>
+              <Col>
+                1 of 2 <Image></Image>
+              </Col>
+              <Col>2 of 2</Col>
+            </Row>
+          </Container>
         </div>
-        {/* </Card.Body>
-        </Card> */}
       </div>
     );
   }
 }
 
-const c3Style = {
+const c2Style = {
   height: "75vh",
   marginTop: "20vh",
 };
 
-export default Contact;
+export default Projects;
