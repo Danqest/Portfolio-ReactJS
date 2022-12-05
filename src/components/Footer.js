f96e60e18e8f4cc8d66a74574a1bb79196dbf613
unknown <mcbrooks@pm.me>
2022-12-04 22:20:53 -0500

    removed unnecessary naming convention

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,22 +2,28 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="text-center" style={{ position: "relative" }}>
+    <footer className="footer-text-center">
       <div className="container">
         <div className="row">
           <div className="col">
             <div className="flex-center">
               <a
+                className="footerLink"
                 href="https://www.linkedin.com/in/mcbrooks6783"
                 target="_blank"
               >
                 LinkedIn
               </a>
               <a> • </a>
-              <a href="https://www.github.com/Danqest" target="_blank">
+              <a
+                className="footerLink"
+                href="https://www.github.com/Danqest"
+                target="_blank"
+              >
                 Github
               </a>
             </div>
+            <a className="footerCloser">This website is built with ReactJS!</a>
           </div>
         </div>
       </div>
