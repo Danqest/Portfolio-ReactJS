da28afae292b611fa430dceb529d3f8892493ba3
unknown <mcbrooks@pm.me>
2022-12-11 11:43:04 -0500

    refactored page, added updated photo, and updated displayed info

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -12,12 +12,41 @@ export class About extends Component {
       <div>
         <div style={c1Style}>
           <h1 id="about">ABOUT ME</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi
-            non quis exercitationem culpa nesciunt nihil aut nostrum explicabo
-            reprehenderit optio amet ab temporibus asperiores quasi cupiditate.
-            Voluptatum ducimus voluptates voluptas?
-          </p>
+          <div className="about-me-content-wrapper">
+            <img
+              className="pfp-img"
+              src="./assets/images/about/pfp-about-me.jpg"
+            />
+            <div className="about-me-text-wrapper">
+              <p>
+                I'm Colin Brooks, a full-stack web developer, Python programmer,
+                and artificial intelligence enthusiast. My technological journey
+                began shortly after university where I worked as a quantitative
+                financial analyst in a small investment firm, gaining
+                professional exposure to database management, data analytics,
+                and the software technologies driving the firm's success. More
+                formally, I'm also a recent graduate of the Georgia Institute of
+                Technology's Full-Stack Web Development program.
+              </p>
+              <p>
+                Over the years since then, I've become engrossed in machine &
+                deep learning, scripting, and programming. Personal projects
+                have included web development (like this website!), creating
+                web-scrapers, and developing AI powered photo classifiers, NLP
+                modules, & investment tools. I am also a co-founder and owner of
+                a startup technology & data company located in Melbourne, FL.
+              </p>
+              <p>
+                Outside of work, my passions include photography, physical
+                fitness, and stock trading. I also enjoy a cappaccino or good
+                scotch every now and then - looking at you Balvenie.
+              </p>
+              <p>
+                To get in contact, feel free to reach out to{" "}
+                <a href="mailto:mcbrooks@pm.me">mcbrooks@pm.me</a>!
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     );
