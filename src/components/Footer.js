331bf62e001bba7922f9d97609af99b2eed6bbd1
unknown <mcbrooks@pm.me>
2022-12-11 11:42:22 -0500

    refactored footer page for scrolling capability

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,30 +2,24 @@ import React from "react";
 
 export default function Footer() {
   return (
-    <footer className="footer-text-center">
-      <div className="container">
-        <div className="row">
-          <div className="col">
-            <div className="flex-center">
-              <a
-                className="footerLink"
-                href="https://www.linkedin.com/in/mcbrooks6783"
-                target="_blank"
-              >
-                LinkedIn
-              </a>
-              <a> • </a>
-              <a
-                className="footerLink"
-                href="https://www.github.com/Danqest"
-                target="_blank"
-              >
-                Github
-              </a>
-            </div>
-            <a className="footerCloser">This website is built with ReactJS!</a>
-          </div>
-        </div>
+    <footer>
+      <div className="footer-container">
+        <a
+          className="footerLink"
+          href="https://www.linkedin.com/in/mcbrooks6783"
+          target="_blank"
+        >
+          LinkedIn
+        </a>
+        <a> • </a>
+        <a
+          className="footerLink"
+          href="https://www.github.com/Danqest"
+          target="_blank"
+        >
+          Github
+        </a>
+        <a className="footer-closer">This website is built with ReactJS!</a>
       </div>
     </footer>
   );
